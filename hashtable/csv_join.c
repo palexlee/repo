22e6153521fc7ae1edb2af809b78b23ceebc38cb
palexlee <pierre.alex.ly@gmail.com>
2016-04-19 01:05:01 +0200

    small bug

--- hashtable/csv_join.c
+++ hashtable/csv_join.c
@@ -14,12 +14,14 @@
  * ======================================================================
  */
 typedef struct bucket{
+	int valid;
     const char* key;
     const void* value;
     struct bucket* next;
 } bucket;
 
 typedef struct {
+	int id;
     size_t size; //taille de l'alvéole
     bucket* content; 
 } alveole;
@@ -142,12 +144,13 @@ Htable* construct_Htable(size_t size) {
 alveole* construct_alveole(const Htable* table, const size_t hash) {
     alveole* a = NULL;
     size_t size = 0;
-    if((table->size - hash) < ALVEOLE_SIZE) {
+    if((table->size - (table->size % ALVEOLE_SIZE)) < hash) {
         /**
          * si la taille de la table n'est pas un multiple de ALVEOLE_SIZE
-         * la dernière alveole de la table aura une taille de (table->size - hash)
+         * la dernière alveole de la table aura une taille de (table->size % ALVEOLE_SIZE)
          */
-        size = table->size - hash; 
+        size = table->size % ALVEOLE_SIZE; 
+        
 
     } else {
         size = ALVEOLE_SIZE;
@@ -176,10 +179,27 @@ alveole* construct_alveole(const Htable* table, const size_t hash) {
 void delete_Htable_and_content(Htable* h) {
     if(h != NULL) {
         if (h->content != NULL) {
+
             for(int i = 0; i < h->nb_alveole; ++i) {
+				
                 if(h->initialized[i] != 0) {
 					h->initialized[i] = 0;
+					
 					if(h->content[i] != NULL) {
+						alveole * a = h->content[i];
+						
+						for(int j = 0; j < a->size; ++j) {
+							bucket* ptr = a->content[j].next;
+							bucket* next = NULL;
+							while(ptr != NULL) {
+								next = ptr->next;
+								free(ptr);
+								ptr = next;
+							}	
+							ptr = NULL;
+							next = NULL;
+						
+						}
 						free((h->content[i])->content);
 					}
                 }
@@ -197,7 +217,6 @@ void add_Htable_value(Htable* h, const char* key, const void* value) {
     if(h != NULL && h->content != NULL) {
         size_t hash = hash_function(key, h->size);
         size_t pos_alveole = hash / ALVEOLE_SIZE;
-        printf("position %zu\n", pos_alveole);
         alveole* a = NULL;
         if(h->initialized[pos_alveole] == 0) {
             a = construct_alveole(h, hash);
@@ -211,7 +230,6 @@ void add_Htable_value(Htable* h, const char* key, const void* value) {
             a = h->content[pos_alveole];
         }
         size_t pos = hash % ALVEOLE_SIZE;
-        printf("alveole: %zu, hash : %zu, modulo: %zu\n",pos_alveole, hash, pos);
         bucket b = {1, key, value, NULL};
         bucket* buck = &a->content[pos];
         if(buck->valid == 0) {
@@ -224,16 +242,25 @@ void add_Htable_value(Htable* h, const char* key, const void* value) {
                 bucket* prev = buck;
                 bucket* curr = buck->next;
                 while(curr != NULL) {
-                    if(curr->key == b.key) {
+                    if(strncmp(curr->key, b.key, 15) == 0) {
                         b.next = curr->next; //update de la valeur pour une clé déjà présente dans la chaine
                         *(prev->next) = b;
+                        printf("chaine %s\n", b.key);
                         break;
                     }
                     prev = curr;
                     curr = curr->next;
                 }
                 if(curr == NULL) { //collision : place à la fin de la chaine
-                    *(prev->next) = b;
+					curr = malloc(sizeof(bucket));
+                    if(curr == NULL) {
+						fprintf(stderr, "Error: not enough memory");
+					} else {
+						*curr = b;
+						prev->next = curr;  //ne pas oublier de free
+					}
+                    printf("collision %s\n", curr->key);
+                    fflush(stdout);
                 }
             }
         }
@@ -241,6 +268,35 @@ void add_Htable_value(Htable* h, const char* key, const void* value) {
     
 }
 
+const void* get_Htable_value(Htable* h, const char* key) {
+	if(h == NULL) {
+		return NULL;
+	}
+	
+	size_t hash = hash_function(key, h->size);
+	size_t pos_alveole = hash / ALVEOLE_SIZE;
+	size_t pos = hash % ALVEOLE_SIZE;
+	
+	alveole* a = h->content[pos_alveole];
+	if(a == NULL) {
+		return NULL;
+	}
+	
+	bucket* b = &a->content[pos];
+	if(b == NULL) {
+		return NULL;
+	}
+	
+	while(b != NULL) {
+		if(strncmp(b->key, key, 15) == 0) {
+			return b->value;
+		} else {
+			b = b->next;
+		}
+	}
+	
+	return NULL;
+}
 
 
 /* ======================================================================
@@ -348,7 +404,109 @@ char* row_element(const csv_const_row row, size_t index)
  * **************************************** */
  
 int hash_join(FILE* in1, FILE* in2, FILE* out, size_t col1, size_t col2, size_t memory) {
- 
+	Htable* table = NULL;
+	int load_factor = (int) memory * HASH_TABLE_LOAD_FACTOR;
+	
+	csv_row current_r1 = read_row(in1); //rajouter une fonction pour trouver la colonne de jointure !!!!!!!!!!!!!!
+	
+	if(current_r1 == NULL) {
+		fprintf(stderr, "Error: can't read file 1");
+		return 1;
+	}
+	
+	csv_row current_r2 = read_row(in2);
+	
+	if(current_r2 == NULL) {
+		fprintf(stderr, "Error: can't read file 2");
+		return 1;
+	}
+	
+	write_rows(out, current_r1, current_r2, 3);
+	
+	if(ferror(out) || feof(out)) {
+		fprintf(stderr,"Error I/O");
+		return 2;
+	}
+		
+	csv_const_row key = NULL;
+	csv_const_row value = NULL;
+	
+	while(current_r1 != NULL || current_r2 != NULL) {
+		table = construct_Htable(4*memory);
+		if(table == NULL) {
+			fprintf(stderr, "Error: can't create the hashtable");
+			delete_Htable_and_content(table);
+			return 2;
+		} 
+		
+		if(current_r1 != NULL) {
+			for(int i = 0; i < load_factor; ++i) {
+				current_r1 = read_row(in1);
+				printf("string length %zu\n", strlen(current_r1));
+				if(current_r1 == NULL || strlen(current_r1) == 0) {
+					current_r1 = NULL;
+					fprintf(stderr, "Error: key not parsed r1\n");
+					break;
+				} else {
+					
+					printf(">%s<\n", current_r1);
+					key = row_element(current_r1, col1);
+					if(key == NULL) {
+						fprintf(stderr, "Error: can't read data key r1\n");
+						delete_Htable_and_content(table);
+						return 1;
+					}
+					printf("key %s\n", key);
+					add_Htable_value(table, key, current_r1);
+				}
+
+			}
+		}
+		
+		
+		if(fseek(in2, 0, SEEK_SET) != 0) {
+			fprintf(stderr, "Error I/O");
+			delete_Htable_and_content(table);
+			return 2;
+		} 
+		
+		if(ferror(in2) || feof(in2)) {
+			fprintf(stderr,"Error I/O");
+			return 2;
+		}
+			
+		current_r2 = read_row(in2);
+	
+		if(current_r2 == NULL) {
+			fprintf(stderr, "Error: can't read file 2");
+			return 1;
+		}
+		while(current_r2 != NULL) {
+			current_r2 = read_row(in2);
+			if(current_r2 != NULL || strlen(current_r2) != 0) {
+				
+				key = row_element(current_r2, col1);
+				if(key == NULL) {
+					fprintf(stderr, "Error: key not parsed r2\n");
+					delete_Htable_and_content(table);
+					return 1;
+				}
+
+				
+				value = get_Htable_value(table, key);/////!!!!!!!
+				printf("value %s\n", value);
+				if(value != NULL) {
+					write_rows(out, value, current_r2, 3);
+				}
+			} else {
+				current_r2 = NULL;
+			}
+		}
+		delete_Htable_and_content(table);
+	}
+	
+	
+	return 0;
 }
 
 
